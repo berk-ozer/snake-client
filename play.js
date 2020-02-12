8b4fe1863cca179c75fb90e97a6ec2c6293836c4
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-12 19:05:01 +0000

    Moved the input handling functions into separate file

--- play.js
+++ play.js
@@ -1,23 +1,6 @@
 const {connect} = require('./client');
-
-const handleUserInput = key => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-
-  return stdin;
-};
-
-
+const {setupInput} = require('./input');
 
 console.log('Connecting ...');
-setupInput();
 connect();
+setupInput();
