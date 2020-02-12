e10ef30c69077bb4fe7501c6e6502fb26397c28a
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-12 18:14:53 +0000

    Initial client setup

---
+++ play.js
@@ -0,0 +1,17 @@
+const net = require('net');
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "172.46.0.236",
+    port: 50541
+  });
+  conn.setEncoding('utf8');
+  conn.on('data', data => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+console.log('Connecting ...');
+connect();
