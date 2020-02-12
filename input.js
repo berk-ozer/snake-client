f299e9d46e56ea41f69d4598ef05d8e6f2e1c3f4
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-12 19:29:03 +0000

    Can take wasd input to move the snake

--- input.js
+++ input.js
@@ -1,10 +1,8 @@
-const handleUserInput = key => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
+let connection;
 
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -12,6 +10,20 @@ const setupInput = () => {
   stdin.on('data', handleUserInput);
 
   return stdin;
+};
+
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'w') {
+    connection.write('Move: up');
+  } else if (key === 'a') {
+    connection.write('Move: left');
+  } else if (key === 's') {
+    connection.write('Move: down');
+  } else if (key === 'd') {
+    connection.write('Move: right');
+  }
 };
 
 module.exports = {setupInput};
