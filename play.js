071ee1393eca76e538778c9f97a58d36d7baf011
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-12 18:18:23 +0000

    Moved connect function into its own file

--- play.js
+++ play.js
@@ -1,17 +1,4 @@
-const net = require('net');
-
-const connect = () => {
-  const conn = net.createConnection({
-    host: "172.46.0.236",
-    port: 50541
-  });
-  conn.setEncoding('utf8');
-  conn.on('data', data => {
-    console.log(data);
-  })
-
-  return conn;
-};
+const {connect} = require('./client');
 
 console.log('Connecting ...');
 connect();
