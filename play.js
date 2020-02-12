32283da92a3e3f3cd399e15bff6e9e108db2b221
Galip Berk Ozer <galipberkozer@gmail.com>
2020-02-12 18:58:56 +0000

    Added functions to handle stdin and  user input

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const {connect} = require('./client');
 
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+
+
 console.log('Connecting ...');
+setupInput();
 connect();
